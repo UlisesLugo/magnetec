bb48c9974b75af003fd32ca56e44cfd053099c89
roberto09 <roberto.gt1509@gmail.com>
2021-05-13 15:46:05 -0500

    Fix Object class

--- js/classes/object.js
+++ js/classes/object.js
@@ -2,5 +2,9 @@ class Object {
     constructor(geometry, material){
         this.geometry = geometry;
         this.material = material;
+        this.object = new THREE.Mesh(geometry, material);
+    }
+    setPosition(posX, posY, posZ){
+        this.object.position.set(posX, posY, posZ);
     }
 }
