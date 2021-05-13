22fddfeeaf1d6b12a4e477469e8cab7200b4f2f6
Ulises Lugo <uliseslf99@hotmail.com>
2021-05-13 15:56:38 -0500

    Added green sphere rotating with reactjs

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from "./logo.svg";
+import "./App.css";
+import * as THREE from "three";
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    let cameraControl;
+    let scene = new THREE.Scene();
+    let camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    let renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // document.body.appendChild( renderer.domElement );
+    // use ref as a mount point of the Three.js scene instead of the document.body
+    document.body.appendChild(renderer.domElement);
+    let geometry = new THREE.SphereGeometry(1);
+    let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    let cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 5;
+    let animate = function () {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+    animate();
+    // EVENT LISTENERS & HANDLERS
+    console.log("Aqui");
+  }, []);
+  return <div className="App"></div>;
+}
+
+export default App;
